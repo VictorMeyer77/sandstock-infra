4f3b9442f71c305847b093d913f6578a58d02e20
VictorMeyer77 <=>
2025-02-15 21:07:54 +0100

    add provider info

--- azure-terraform/main.tf
+++ azure-terraform/main.tf
@@ -21,7 +21,9 @@ provider "azurerm" {
 }
 
 provider "azuread" {
-  tenant_id = var.tenant_id
+  tenant_id     = var.tenant_id
+  client_id     = var.deploy_client_id
+  client_secret = var.deploy_client_secret
 }
 
 data "azurerm_client_config" "current" {} # used ?
