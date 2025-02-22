5d7bd1c4e922e42395fa7e80bd85558fb6e0d523
VictorMeyer77 <=>
2025-02-22 17:30:17 +0100

    add key vault

--- processing.tf
+++ processing.tf
@@ -24,8 +24,15 @@ resource "azurerm_data_factory_integration_runtime_azure" "adf_runtime" {
 resource "azurerm_data_factory_linked_service_azure_sql_database" "adf_sql" {
   name                     = "${var.environment}-${var.project}-adf-sql"
   data_factory_id          = azurerm_data_factory.adf.id
-  connection_string        = "data source=${azurerm_mssql_server.sql_server.fully_qualified_domain_name},1433;initial catalog=${azurerm_mssql_database.erp_db.name};user id=sql_admin;Password=${var.sql_db_admin_password};integrated security=False;encrypt=True;connection timeout=30"
   integration_runtime_name = azurerm_data_factory_integration_runtime_azure.adf_runtime.name
+
+  connection_string = "Server=tcp:${azurerm_mssql_server.sql_server.fully_qualified_domain_name},1433;Database=${azurerm_mssql_database.erp_db.name};User ID=sql_admin;Encrypt=True;Connection Timeout=30;"
+
+  key_vault_password {
+    linked_service_name = azurerm_data_factory_linked_service_key_vault.adf_kv.name
+    secret_name         = azurerm_key_vault_secret.db_usr_pwd.name
+  }
+
 }
 
 resource "azurerm_data_factory_linked_service_azure_blob_storage" "adf_sto" {
@@ -36,6 +43,13 @@ resource "azurerm_data_factory_linked_service_azure_blob_storage" "adf_sto" {
 
   use_managed_identity = true
 
+}
+
+resource "azurerm_data_factory_linked_service_key_vault" "adf_kv" {
+  name                     = "${var.environment}-${var.project}-adf-kv"
+  data_factory_id          = azurerm_data_factory.adf.id
+  key_vault_id             = azurerm_key_vault.kv.id
+  integration_runtime_name = azurerm_data_factory_integration_runtime_azure.adf_runtime.name
 }
 
 # Datasets
--- storage.tf
+++ storage.tf
@@ -21,3 +21,58 @@ resource "azurerm_storage_container" "container" {
   storage_account_id    = azurerm_storage_account.storage.id
   container_access_type = "private"
 }
+
+resource "azurerm_key_vault" "kv" {
+  name                        = "${var.environment}-${var.project}-kv"
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  sku_name                    = "standard"
+  tags                        = var.tags
+}
+
+# Access Policies
+
+resource "azurerm_key_vault_access_policy" "adf_kv_policy" {
+  key_vault_id = azurerm_key_vault.kv.id
+
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = data.azurerm_client_config.current.object_id
+
+  key_permissions = [
+    "Create",
+    "Get",
+  ]
+
+  secret_permissions = [
+    "Set",
+    "Get",
+    "Delete",
+    "Purge",
+    "Recover"
+  ]
+}
+
+resource "azurerm_key_vault_access_policy" "adf_kv_policy" {
+  key_vault_id = azurerm_key_vault.kv.id
+
+  tenant_id = azurerm_data_factory.adf.identity[0].tenant_id
+  object_id = azurerm_data_factory.adf.identity[0].principal_id
+
+  secret_permissions = [
+    "Get", "List"
+  ]
+}
+
+
+# Secrets
+
+resource "azurerm_key_vault_secret" "db_usr_pwd" {
+  name         = "${azurerm_mssql_server.sql_server.name}-password"
+  content_type = "Password of ${azurerm_mssql_server.sql_server.administrator_login} on azure sql ${azurerm_mssql_server.sql_server.name}"
+  value        = var.sql_db_admin_password
+  key_vault_id = azurerm_key_vault.kv.id
+}
